114a4a6dd7e31062832e56db0965d7aa2a537823
brunojurakic <bruno.jurakic@fer.unizg.hr>
2025-01-07 22:40:49 +0100

    finish login page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import Login from "../components/Login.jsx";
-import Dashboard from "../components/Dashboard.jsx";
+import Login from "./components/Login.jsx";
+import Dashboard from "./components/Dashboard.jsx";
+import './styles/App.css'
 
 function App() {
   return (
