eb511034427194ca64c8f22abe0ef1b9ecf002c9
brunojurakic <bruno.jurakic@fer.unizg.hr>
2025-01-07 16:47:30 +0100

    init

---
+++ jollybringer/src/test/java/hr/fer/jollybringer/JollybringerApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.jollybringer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JollybringerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
