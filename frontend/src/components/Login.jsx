f07b6e67d087ca0d330658cbc60dbfd33112dbb5
brunojurakic <bruno.jurakic@fer.unizg.hr>
2025-01-08 20:18:00 +0100

    fix bg css

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -8,8 +8,8 @@ const Login = () => {
   }
 
   return (
-    <div className={'flex justify-center h-dvh items-center'}>
-      <div className={'border border-white rounded-[8px] p-12 flex flex-col justify-center items-center gap-7 backdrop-blur bg-black bg-opacity-50'}>
+    <div className={'login-background flex justify-center h-dvh items-center'}>
+      <div className={'login-wrapper border border-white rounded-[8px] p-12 flex flex-col justify-center items-center gap-7 backdrop-blur bg-black bg-opacity-50'}>
         <h1 className={'text-4xl text-white'}>Login</h1>
         <button
           className={'text-white border-green-600 bg-green-800 border rounded-[8px] p-[8px] px-6 hover:bg-green-700 transition duration-200'}
